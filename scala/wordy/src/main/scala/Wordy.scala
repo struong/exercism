f1e4868cfc492f5d253c791f75cb3212000aaf96
Steven Truong <>
2022-10-03 12:23:36 +0100

    wordy solved

--- scala/wordy/src/main/scala/Wordy.scala
+++ scala/wordy/src/main/scala/Wordy.scala
@@ -1,26 +1,38 @@
+import cats.data.{NonEmptyList => Nel}
 import cats.parse.Numbers.signedIntString
 import cats.parse.Parser
-// a collection of useful parsers
+import cats.parse.Parser.{string => pstring}
 import cats.parse.Rfc5234._
-import cats.parse.Parser.{string => pstring, char => pchar}
+
 import scala.language.postfixOps
 
 object Wordy {
-  final case class Op(l: Int, operator: String, r: Int)
+  final case class Op(firstDigit: Int, operations: Nel[(String, Int)])
 
   def answer(input: String): Option[Int] = {
 
+    val whatIsParser = (pstring("What is") ~ sp).void
     val digitsParser = signedIntString.map(_.toInt)
-    val opParser = (Parser.string("plus") | Parser.string("minus")).surroundedBy(sp).string
-    val whatIsParser = pstring("What is") ~ sp
-    val parser: Parser[Op] = (digitsParser ~ opParser ~ digitsParser).map(Op.tupled)
 
-    println(s"parser.parse(input) = ${ parser.parse(input)}")
-    
-    val result = parser.parse(input).map {
-      case (rest, plus) =>
-        println(s"rest = ${rest}")
-        plus.r + plus.l
+    val opParser =
+      (Parser.string("plus") | Parser.string("minus") | Parser.string(
+        "divided by"
+      ) | Parser.string(
+        "multiplied by"
+      )).surroundedBy(sp).string ~ digitsParser
+
+    val parser =
+      ((whatIsParser *> digitsParser) ~ opParser.rep).map(Op.tupled)
+
+    val result = parser.parse(input).map { case (_, op) =>
+      op.operations.foldLeft(op.firstDigit) { case (accum, (operator, value)) =>
+        operator.trim match {
+          case "plus"          => accum + value
+          case "minus"         => accum - value
+          case "multiplied by" => accum * value
+          case "divided by"    => accum / value
+        }
+      }
     }
 
     result.toOption
